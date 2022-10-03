c30f5fdab419d13ada384cc99440812f6ace087e
ademberk90 <ademberk90@gmail.com>
2022-10-03 20:43:00 +0300

    eslint errors fixed v4

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 import Layout from '@/components/Layout'
 import { useAppSelector, useAppDispatch } from '../hooks'
-import { decrement, increment } from '../slices/test'
+
 
 export default function Home() {
   // const count = useAppSelector((state) => state.counter.value)
