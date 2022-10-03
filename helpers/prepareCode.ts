e1885b7bbfd2642fab52fb3d3657806f254d88b1
ademberk90 <ademberk90@gmail.com>
2022-10-03 22:13:49 +0300

    prepareCode edited

--- helpers/prepareCode.ts
+++ helpers/prepareCode.ts
@@ -3,7 +3,7 @@ import { ModalState } from "../slices/modal"
 const  prepareCode = (modalObj: ModalState): string =>  {
     console.log(modalObj);
     let sendObj = JSON.stringify(modalObj);
-    let code = `<script src="https://cdn.tailwindcss.com"></script><script src="http://localhost:3000/modalFunctions/modal${modalObj.modal}.js"></script><script>window.start.init(${sendObj})</script>`;
+    let code = `<script src="https://cdn.tailwindcss.com"></script><script src="${modalObj.baseUrl}/modalFunctions/modal${modalObj.modal}.js"></script><script>window.start.init(${sendObj})</script>`;
     console.log(code);
     return code;
 }
