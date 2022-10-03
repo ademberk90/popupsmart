e1942e5fda6ef2602ab006d96084098dacc4878a
ademberk90 <ademberk90@gmail.com>
2022-10-03 20:41:03 +0300

    eslint errors fixed v3

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import fs from 'fs'
 import path from 'path'
-import formidable, { File } from 'formidable'
+
 import FormData from 'form-data'
-import mv from 'mv'
+// import formidable, { File } from 'formidable'
+const formidable = require("formidable")
+// import mv from 'mv'
+const mv = require("mv")
 
 type ProcessedFiles = Array<[string, File]>
 
@@ -20,15 +23,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   /* Get files using formidable */
   const files = await new Promise<ProcessedFiles | undefined>((resolve, reject) => {
+    // eslint-disable-next-line no-undef
     const form = new formidable.IncomingForm()
+    // eslint-disable-next-line no-shadow
     const files: ProcessedFiles = []
-    form.on('file', (field, file) => {
+    form.on('file', (field: string, file: File) => {
       files.push([field, file])
     })
     form.on('end', () => resolve(files))
-    form.on('error', (err) => reject(err))
+    form.on('error', (err: any) => reject(err))
 
-    form.parse(req, (err, fields, files) => {
+    // eslint-disable-next-line consistent-return, no-shadow
+    form.parse(req, (err: any, fields: any, files: { file: { filepath: any; originalFilename: string } }) => {
         
       if (err) return reject(err)
       console.log(fields, files)
@@ -37,7 +43,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       const fName = req.headers.type === 'logo' ? 'logo' : 'image'
       const rndm = Date.now() + (Math.random() + 1).toString(36).substring(2) 
       const newPath = `./public/upload/${fName}_${rndm}.${files.file.originalFilename.split('.')[1].toLowerCase()}`
-      mv(oldPath, newPath, function (err) {})
+      // eslint-disable-next-line no-shadow
+      mv(oldPath, newPath, () => {})
       res.status(200).json({ fields, files, newPath })
     })
   }).catch((e) => {
